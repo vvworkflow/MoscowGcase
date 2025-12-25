277ab1ed5c4635ddf763490dd631fe584bd3df0e
vans1r <serebrogml@gmail.com>
2025-12-25 15:01:29 +0300

    edit validation for russian mobile phone numbers

--- src/phone.py
+++ src/phone.py
@@ -8,7 +8,7 @@ def phone_normalization(raw_phone: str) -> str:
 
 def phone_number_validation(raw_phone: str) -> bool:
     n = re.sub(r"\D", '', raw_phone)
-    if (n.startswith('7') or n.startswith('8')) and len(n) == 11:
+    if (n.startswith('79') or n.startswith('89')) and len(n) == 11:
         return True
     else:
         return False
